8493ec1b508362ff3d1045ed32e99db233d74efc
Michał <michkam96@gmail.com>
2023-05-03 15:46:57 +0200

    hook fixed

--- src/hooks/useMovieData.js
+++ src/hooks/useMovieData.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react"
 
 export const useMovieData = (query) => {
-    const [movie, setMovie] = useState();
+    const [movie, setMovie] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
-useEffect(() => {
-    setLoading(true)
-    getData();
-},[])
+    useEffect(() => {
+        setLoading(true)
+        getData();
+    }, [])
 
 
     const getData = async () => {
         const key = process.env.REACT_APP_API_KEY;
-       
-        try{
+
+        try {
             const res = await fetch(`https://api.themoviedb.org/3/${query}?api_key=${key}`)
             const data = await res.json();
             setMovie(data)
-        } catch(er){
+        } catch (er) {
             setError(er.message)
         }
         setLoading(false)
@@ -26,10 +26,10 @@ useEffect(() => {
 
 
 
-return {
-    movie,
-    loading,
-    error
-}
+    return {
+        movie,
+        loading,
+        error
+    }
 
 }
